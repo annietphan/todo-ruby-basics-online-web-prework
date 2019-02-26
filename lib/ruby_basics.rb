7881b0c873fbe05765d77cd4eca32b0254d6728b
Annie Phan <anniephan.atp@gmail.com>
2019-02-26 07:30:38 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -12,8 +12,9 @@ def argue(phrase = "I'm right and you are wrong!")
   phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting = "Hi there, "
+  name = "Bobby!"
 end
 
 def return_a_value
